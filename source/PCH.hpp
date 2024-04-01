13e90ba94a72ef7005e3269da67c09bebadf7253
EddieBreeg <eddiebreeg0@protonmail.com>
2024-04-01 09:53:15 -0400

    added utility and integral typedefs

--- source/PCH.hpp
+++ source/PCH.hpp
@@ -5,4 +5,14 @@
 #include <utility>
 #include <new>
 
+#include <core/Utility.hpp>
 #include <math/Vector.hpp>
+
+using int8 = int8_t;
+using uint8 = uint8_t;
+using int16 = int16_t;
+using uint16 = uint16_t;
+using int32 = int32_t;
+using uint32 = uint32_t;
+using int64 = int64_t;
+using uint64 = uint64_t;
---
+++ source/core/Utility.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <type_traits>
+
+namespace breakout {
+	template <class T, class... U>
+	constexpr auto Max(T&& left, U&&... right) noexcept
+	{
+		static_assert((std::is_same_v<std::decay_t<T>, std::decay_t<U>> && ...),
+					  "All values must have the same type");
+		T res = std::forward<T>(left);
+
+		return ((res = res > right ? res : std::forward<U>(right)), ...);
+	}
+
+	template <class T, class... U>
+	constexpr auto Min(T&& left, U&&... right) noexcept
+	{
+		static_assert((std::is_same_v<std::decay_t<T>, std::decay_t<U>> && ...),
+					  "All values must have the same type");
+		T res = std::forward<T>(left);
+
+		return ((res = res < right ? res : std::forward<U>(right)), ...);
+	}
+} // namespace breakout
