c1d14bc97f296cc6f9b7fa9c18ece703f10373be
EddieBreeg <eddiebreeg0@protonmail.com>
2024-04-29 18:37:08 -0400

    re structured UTs

--- tests/core/main.cpp
+++ tests/core/main.cpp
@@ -8,6 +8,12 @@ namespace brk::utf8::ut
 	void Utf8Tests();
 } // namespace brk::utf8::ut
 
+namespace brk::ulid::ut
+{
+	void ULIDTests();
+} // namespace brk::ulid::ut
+
+
 int main(int argc, char const *argv[])
 {
 	brk::core::ut::EventTests();
