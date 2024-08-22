98fc3101a2e46f913ac412a5e406c9759d42de26
EddieBreeg <eddiebreeg0@protonmail.com>
2024-08-22 16:23:19 +0200

    quaternions (?)

--- tests/math/main.cpp
+++ tests/math/main.cpp
@@ -1,8 +1,10 @@
 namespace math::ut {
 	void VectorTests();
+	void QuaternionRotationTests();
 } // namespace math::ut
 
 int main(int argc, char const* argv[])
 {
 	math::ut::VectorTests();
+	math::ut::QuaternionRotationTests();
 }
