925583f30f6cc16c15b184c709cef0aaebfb8b60
EddieBreeg <eddiebreeg0@protonmail.com>
2024-11-22 18:22:34 +0100

    fixed hilarious bug causing all system instances to be nullptr

--- source/ecs/System.cpp
+++ source/ecs/System.cpp
@@ -2,15 +2,16 @@
 #include <core/Assert.hpp>
 
 brk::ecs::SystemInstance::SystemInstance(SystemInstance&& other)
-	: m_Update{ other.m_Update }
+	: m_SystemPtr{ other.m_SystemPtr }
+	, m_Update{ other.m_Update }
 	, m_Terminate{ other.m_Terminate }
 {
+	other.m_SystemPtr = nullptr;
 	other.m_Update = nullptr;
 	other.m_Terminate = nullptr;
 }
 
-void brk::ecs::SystemInstance::Update(entt::registry& world,
-										   const TimeInfo& timeInfo)
+void brk::ecs::SystemInstance::Update(entt::registry& world, const TimeInfo& timeInfo)
 {
 	BRK_ASSERT(m_Update, "Tried to call Update on an invalid system instance");
 	m_Update(m_SystemPtr, world, timeInfo);
