e6f93150293dae36d01493533c1601cfe28a2ddc
EddieBreeg <eddiebreeg0@protonmail.com>
2024-12-19 21:19:54 +0100

    aded TrackerAllocator

--- source/core/AllocTracker.hpp
+++ source/core/AllocTracker.hpp
@@ -39,4 +39,59 @@ namespace brk {
 		std::pmr::memory_resource* m_Upstream = std::pmr::new_delete_resource();
 		Info m_Info;
 	};
+
+	/**
+	 * An STL compliant allocator, which used an AllocTracker as its backend.
+	 * This is a convenience API for use with STL containers.
+	 */
+	template <class T>
+	struct TrackerAllocator
+	{
+		using value_type = T;
+		using is_always_equal = std::false_type;
+		using propagate_on_container_move_assignment = std::true_type;
+
+		template <class U>
+		struct rebind
+		{
+			using other = TrackerAllocator<U>;
+		};
+
+		explicit TrackerAllocator(AllocTracker& upstream) noexcept
+			: m_Upstream{ &upstream }
+		{}
+
+		template <class U>
+		TrackerAllocator(const TrackerAllocator<U>& other) noexcept
+			: m_Upstream{ other.m_Upstream }
+		{}
+
+		TrackerAllocator(const TrackerAllocator&) noexcept = default;
+		TrackerAllocator& operator=(const TrackerAllocator&) noexcept = default;
+		~TrackerAllocator() = default;
+
+		[[nodiscard]] bool operator==(const TrackerAllocator& other) const noexcept
+		{
+			return m_Upstream == other.m_Upstream;
+		}
+		[[nodiscard]] bool operator!=(const TrackerAllocator& other) const noexcept
+		{
+			return m_Upstream != other.m_Upstream;
+		}
+
+		T* allocate(size_t n)
+		{
+			return static_cast<T*>(m_Upstream->allocate(n * sizeof(T), alignof(T)));
+		}
+		void deallocate(T* ptr, size_t n)
+		{
+			m_Upstream->deallocate(ptr, n * sizeof(T), alignof(T));
+		}
+
+		template <class U>
+		friend struct TrackerAllocator;
+
+	private:
+		AllocTracker* m_Upstream;
+	};
 } // namespace brk
